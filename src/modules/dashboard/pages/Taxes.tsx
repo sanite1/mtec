e4eab53e519eab914cc604f880e5dff85841a14c
sanite1 <csanni52@gmail.com>
2025-11-12 13:06:32 +0100

    feat taxes routes

--- src/modules/dashboard/pages/Taxes.tsx
+++ src/modules/dashboard/pages/Taxes.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import TaxHeader from "../components/taxes/TaxHeader";
 import TaxTable from "../components/taxes/TaxTable";
 import { getDecodedJwt } from "../lib/auth";
-import { TaxFilters } from "../lib/types/taxes";
 import { useStoreTaxes } from "../lib/api/taxes";
 
 export default function Taxes() {
