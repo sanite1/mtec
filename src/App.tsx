16e39d899aa92736d29e03d85e807ea0c114966e
sanite1 <csanni52@gmail.com>
2025-11-26 17:32:47 +0100

    fix: query provider fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import RoutesWrapper from "./routes/Wrapper";
 import { Toaster } from "sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App: React.FC = () => {
+  const queryClient = new QueryClient();
   return (
     <div>
-      <Toaster richColors position="bottom-right" />
-      <RoutesWrapper />
+      <QueryClientProvider client={queryClient}>
+        <Toaster richColors position="bottom-right" />
+        <RoutesWrapper />
+      </QueryClientProvider>
     </div>
   );
 };
